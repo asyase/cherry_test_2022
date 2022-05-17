6a3cfa9a604eaa0a57b46eb74032a44561bd9b20
Ihavenoimagination <e1327973@student.tuwien.ac.at>
2022-05-17 15:08:06 +0200

    update

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,20 @@ window.addEventListener('load', (event) => {
         this.classList.toggle("bg-red");
 
     })
+
+    document.getElementById("burger").addEventListener("click",function ()
+    {
+        this.classList.add("hidden");
+        document.getElementById("close-img").classList.remove("hidden");
+        document.getElementById("mob-menu").classList.remove("hidden");
+
+    })
+
+    document.getElementById("close-img").addEventListener("click",function ()
+    {
+        this.classList.add("hidden");
+        document.getElementById("burger").classList.remove("hidden");
+        document.getElementById("mob-menu").classList.add("hidden");
+
+    })
 });
